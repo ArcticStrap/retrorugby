de51a51dad71d46c1763fbc5d4872410d78f47ed
ChaosIsFramecode <framecodechaos@gmail.com>
2025-01-03 14:47:49 -0500

    Basic window

--- makefile
+++ makefile
@@ -4,7 +4,7 @@ all: build
 
 build:
 	mkdir -p bin
-	gcc -o bin/retrorugby src/main.c
+	gcc -o bin/retrorugby src/main.c -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
 
 clear:
 	rm -rf bin
--- src/main.c
+++ src/main.c
@@ -1,3 +1,25 @@
-int main(void) {
+#include "raylib.h"
 
+int main(void)
+{
+    const int screenWidth = 1280;
+    const int screenHeight = 720;
+
+    InitWindow(screenWidth, screenHeight, "Retro Rugby");
+
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+
+        EndDrawing();
+    }
+
+    CloseWindow();        // Close window and OpenGL context
+
+    return 0;
 }
